3363006185b94e4388080ce2cd620f4d97a32f6e
Kelly Santos <kellycristinasntos@gmail.com>
2022-10-23 10:35:10 -0300

    Pagination adjusted

--- index.js
+++ index.js
@@ -16,34 +16,102 @@ app.use(
   })
 );
 app.get("/", async (req, res) => {
-  const product = await ProductModel.find({});
-  res.status(201).json(product);
+  const page = req.query.page || 0
+  let limit = req.query.limit || 8
+  let itemsTotal = await ProductModel.find({}).count()
+ 
+
+  const product = await ProductModel.find({})
+  .skip(page * limit )
+  .limit(limit)
+
+  let totalPages = parseInt(itemsTotal / limit)
+  const data = {itemsTotal, limit,page,totalPages, product}
+  res.status(201).json(data);
 });
 
 app.get("/name/:flavor", async (req, res) => {
+  const page = req.query.page || 0
+  let limit = req.query.limit || 5
+  let itemsTotal = await ProductModel.find({}).count()
+
   const regex = new RegExp(req.params.flavor, "i");
-  const product = await ProductModel.find({ name: { $regex: regex } });
-  res.status(201).json(product);
+  const product = await ProductModel.find({ name: { $regex: regex } })
+  .skip(page * limit )
+  .limit(limit);
+  
+  let returnTotal = await ProductModel.find({ name: { $regex: regex } }).count()
+  let totalPages = parseInt(returnTotal / limit)
+  const data = {itemsTotal, returnTotal, limit,page,totalPages, product}
+  res.status(201).json(data);
 });
 
 app.get("/drink", async (req, res) => {
-  const product = await ProductModel.find({ category: "drink" });
-  res.status(201).json(product);
+  const page = req.query.page || 0
+  let limit = req.query.limit || 5
+  let itemsTotal = await ProductModel.find({}).count()
+  
+
+  const product = await ProductModel.find({ category: "drink" })
+  .skip(page * limit )
+  .limit(limit);
+
+  let returnTotal = await ProductModel.find({ category: "drink" }).count()
+  let totalPages = parseInt(returnTotal / limit)
+  const data = {itemsTotal, returnTotal, limit,page,totalPages, product}
+  res.status(201).json(data);
 });
 
 app.get("/burger", async (req, res) => {
-  const product = await ProductModel.find({ category: "burger" });
-  res.status(201).json(product);
+  const page = req.query.page || 0
+  let limit = req.query.limit || 5
+  let itemsTotal = await ProductModel.find({}).count()
+  
+
+  const product = await ProductModel.find({ category: "burger" })
+    .skip(page * limit )
+  .limit(limit);
+  
+
+  let returnTotal = await ProductModel.find({ category: "burger" }).count()
+  let totalPages = parseInt(returnTotal / limit)
+  const data = {itemsTotal, returnTotal, limit,page,totalPages, product}
+  res.status(201).json(data);
 });
 
 app.get("/dessert", async (req, res) => {
-  const product = await ProductModel.find({ category: "dessert" });
-  res.status(201).json(product);
+  const page = req.query.page || 0
+  let limit = req.query.limit || 5
+  let itemsTotal = await ProductModel.find({}).count()
+  
+
+  const product = await ProductModel.find({ category: "dessert" })
+    .skip(page * limit )
+  .limit(limit);
+  
+
+  let returnTotal = await ProductModel.find({ category: "dessert" }).count()
+  let totalPages = parseInt(returnTotal / limit)
+  const data = {itemsTotal, returnTotal, limit,page,totalPages, product}
+  res.status(201).json(data);
+
 });
 
 app.get("/side", async (req, res) => {
-  const product = await ProductModel.find({ category: "side" });
-  res.status(201).json(product);
+  const page = req.query.page || 0
+  let limit = req.query.limit || 5
+  let itemsTotal = await ProductModel.find({}).count()
+
+
+  const product = await ProductModel.find({ category: "side" })
+    .skip(page * limit )
+  .limit(limit);
+  
+
+  let returnTotal = await ProductModel.find({ category: "side" }).count()
+  let totalPages = parseInt(returnTotal / limit)
+  const data = {itemsTotal, returnTotal, limit,page,totalPages, product}
+  res.status(201).json(data);
 });
 
 connect();
